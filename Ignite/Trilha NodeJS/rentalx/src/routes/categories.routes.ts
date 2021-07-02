f562a36e92d22b56df30b957b5ed59d42b5e86f5
Alysson <alysson6801@hotmail.com>
2021-07-02 09:43:31 -0300

    chore: import category use case

--- Ignite/Trilha NodeJS/rentalx/src/routes/categories.routes.ts
+++ Ignite/Trilha NodeJS/rentalx/src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import multer from 'multer'
 
 const upload = multer({
@@ -12,9 +13,7 @@ const categoriesRoutes = Router()
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response))
 categoriesRoutes.get("/", (request, response) =>  listCategoriesController.handle(request, response))
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  const { file } = request
-  console.log(file)
-  return response.send()
+  return importCategoryController.handle(request, response)
 })
 
 export { categoriesRoutes }
