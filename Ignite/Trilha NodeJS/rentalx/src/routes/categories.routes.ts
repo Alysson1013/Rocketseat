6e5bc9923bcddcaddc77573fcec63b08bbce1ca2
Alysson <alysson6801@hotmail.com>
2021-07-02 09:22:52 -0300

    chore: file import

--- Ignite/Trilha NodeJS/rentalx/src/routes/categories.routes.ts
+++ Ignite/Trilha NodeJS/rentalx/src/routes/categories.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import multer from 'multer'
+
+const upload = multer({
+  dest: "./tmp", 
+})
 
 const categoriesRoutes = Router()
 
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response))
 categoriesRoutes.get("/", (request, response) =>  listCategoriesController.handle(request, response))
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request
+  console.log(file)
+  return response.send()
+})
 
 export { categoriesRoutes }
