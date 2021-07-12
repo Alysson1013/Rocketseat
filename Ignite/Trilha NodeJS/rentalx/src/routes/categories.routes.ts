0267ecc69d8412b9be8f072b5ad91f942b77e1f6
Alysson Eduardo <alysson6801@hotmail.com>
2021-07-12 10:54:08 -0300

    refactor: dependencies injection with tsyringe

--- Ignite/Trilha NodeJS/rentalx/src/routes/categories.routes.ts
+++ Ignite/Trilha NodeJS/rentalx/src/routes/categories.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
-import createCategoryController from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import multer from 'multer'
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 
 const upload = multer({
   dest: "./tmp", 
 })
 
+const createCategoryController = new CreateCategoryController()
+
 const categoriesRoutes = Router()
 
-categoriesRoutes.post('/', (request, response) => createCategoryController().handle(request, response))
+categoriesRoutes.post('/', createCategoryController.handle)
 categoriesRoutes.get("/", (request, response) => listCategoriesController.handle(request, response))
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => importCategoryController.handle(request, response))
 
--- Ignite/Trilha NodeJS/rentalx/src/server.ts
+++ Ignite/Trilha NodeJS/rentalx/src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
-import swaggerUi from "swagger-ui-express"
-import "./database"
+import swaggerUi from "swagger-ui-express";
+import "./database";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-
-
+import "./shared/container";
 
 const app = express();
 
-app.use(express.json());
-
+app.use(express.json()); 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
