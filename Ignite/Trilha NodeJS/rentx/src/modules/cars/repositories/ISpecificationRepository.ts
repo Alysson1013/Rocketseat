7f63abdaba096a7b9e2398b95069b4d0aa36e074
Alysson Eduardo <alysson6801@hotmail.com>
2021-07-15 10:44:16 -0300

    chore: documentation update

---
+++ Ignite/Trilha NodeJS/rentx/src/modules/cars/repositories/ISpecificationRepository.ts
@@ -0,0 +1,16 @@
+import { Specification } from "../entities/Specification";
+
+interface ISpecificationRepositoryDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationRepository {
+  create({ name, description }: ISpecificationRepositoryDTO): Promise<void>
+  findByName(name: string): Promise<Specification>
+}
+
+export {
+  ISpecificationRepository,
+  ISpecificationRepositoryDTO
+}
