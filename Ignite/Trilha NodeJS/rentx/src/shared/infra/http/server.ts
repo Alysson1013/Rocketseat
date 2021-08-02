17d194a73ce7a4b9056c1124230915ba463d44d3
Alysson Eduardo <alysson6801@hotmail.com>
2021-08-02 09:18:55 -0300

    chore: create car migration

--- Ignite/Trilha NodeJS/rentx/src/shared/infra/http/server.ts
+++ Ignite/Trilha NodeJS/rentx/src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors"
 import swaggerUi from "swagger-ui-express";
-import "../database";
+import "../typeorm";
 import { router } from "./routes";
 import swaggerFile from "../../../swagger.json";
 
